886ebb0b125f04145fb14a9c24f55479596e634b
Nadya Popova <71140899+nodlya@users.noreply.github.com>
2023-12-02 23:30:52 +0500

    проброс значения в родителя

--- src/pages/JavaMan.vue
+++ src/pages/JavaMan.vue
@@ -1,26 +1,28 @@
 <template>
-  <div id="app">
-    <vue-fake-input
-      :length="5"
-      :fontSize="40"
-      v-model="fkValue"
-    />
-    <h2>Value: {{fkValue}}</h2>
-  </div>
-</template>
-
-<script>
-import VueFakeInput from '../components/VueFakeInput.vue';
-
-export default {
-  name: 'JavaMan',
-  components: {
-    VueFakeInput,
-  },
-  data() {
-    return {
-      fkValue: '',
-    };
+    <div id="app">
+      <CustomInput :len="5" @fkValueChanged="handleFkValueChanged" />
+      <h2>Value: {{ fkValue }}</h2>
+    </div>
+  </template>
+  
+  <script>
+  import CustomInput from '../components/CustomInput.vue'
+  
+  export default {
+    name: 'JavaMan',
+    components: {
+      CustomInput
+    },
+    data() {
+      return {
+        fkValue: ''
+      }
+    },
+    methods: {
+      handleFkValueChanged(value) {
+        this.fkValue = value;
+      }
+    }
   }
-};
-</script>
+  </script>
+  
