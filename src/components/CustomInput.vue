297fd60ea0eed4c4447d7505890f9a0e6726602c
Nadya Popova <71140899+nodlya@users.noreply.github.com>
2023-12-03 11:32:32 +0500

    jJavaMan w/o styles but logic

--- src/components/CustomInput.vue
+++ src/components/CustomInput.vue
@@ -1,36 +1,34 @@
 <template>
-    <div id="app">
-      <vue-fake-input :length="len" :fontSize="40" v-model="fkValue" @input="updateFkValue" />
-    </div>
-  </template>
-  
-  <script>
-  import VueFakeInput from '../components/VueFakeInput.vue'
-  
-  export default {
-    name: 'CustomInput',
-    components: {
-      VueFakeInput
-    },
-    props: {
-      len: Number
-    },
-    data() {
-      return {
-        fkValue: ''
+  <div id="app">
+    <vue-fake-input :length="len" :fontSize="40" v-model="fkValue" @input="updateFkValue" />
+  </div>
+</template>
+
+<script>
+import VueFakeInput from '../components/VueFakeInput.vue'
+
+export default {
+  name: 'CustomInput',
+  components: {
+    VueFakeInput
+  },
+  props: {
+    len: Number
+  },
+  data() {
+    return {
+      fkValue: ''
+    }
+  },
+  methods: {
+    updateFkValue(event) {
+      if (event?.inputType === 'deleteContentBackward') {
+        this.fkValue = this.fkValue.substring(0, this.fkValue.length - 1);
+      } else {
+        this.fkValue += event.target.value;
       }
-    },
-    methods: {
-      updateFkValue(event) {
-        if (event?.inputType === 'deleteContentBackward') {
-          this.fkValue = this.fkValue.substring(0, this.fkValue.length - 1);
-        } else {
-          this.fkValue += event.target.value;
-        }
-        // Посылаем кастомное событие в родительский компонент
-        this.$emit('fkValueChanged', this.fkValue);
-      }
+      this.$emit('fkValueChanged', this.fkValue);
     }
   }
-  </script>
-  
+}
+</script>
